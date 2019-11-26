74a7225b88004c768429827d81b7ef07abfdc0f5
ZQA <zkvpz@naver.com>
2019-11-26 15:58:54 +0900

    - 프로젝트 세팅  : 패키지 네임 변경.  : 샘플 씬 제거.  : 테스트씬, 테스트 스크립트 추가.  : 빌드 씬 변경. (샘플씬 -> 테스트씬).

---
+++ Assets/Project/_Test/TestScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+// .
+
+public class TestScript : MonoBehaviour
+{
+
+	private void Awake()
+	{
+
+	}
+
+	private void Start()
+	{
+
+	}
+
+	private void Update()
+	{
+
+	}
+
+}
